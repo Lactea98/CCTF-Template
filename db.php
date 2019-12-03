54625688a3988749086a23ea3b195e430ff236d6
Lactea98 <whdals7979@naver.com>
2019-12-03 20:40:37 +0900

    CCTF Template

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'localhost';
-    $user = 'universe';
-    $pw = 'kk123456789';
+    $user = 'aaaaaa';
+    $pw = 'aaaaaaaa';
     $dbname = 'CCTF';
     $mysqli = mysqli_connect($host, $user, $pw, $dbname);
     
